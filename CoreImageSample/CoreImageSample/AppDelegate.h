e763f59ab16470288927e39c03b96aaa040749e5
ito masaya <itomasaya@team-lab.com>
2012-09-20 19:29:16 +0900

    CIFilterを使ってみた。モノクロだけですが。

---
+++ CoreImageSample/CoreImageSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CoreImageSample
+//
+//  Created by masaya ito on 12/09/20.
+//  Copyright (c) 2012年 mIto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CoreImageSample/CoreImageSample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CoreImageSample
+//
+//  Created by masaya ito on 12/09/20.
+//  Copyright (c) 2012年 mIto. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
